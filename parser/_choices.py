e7c35e9ec3e4ca603e512524dd7d6e2a1ff0a8db
dk949 <david2000katz@gmail.com>
2020-11-29 02:34:00 +0000

    now parser works. But all variables need to be moved into the class, so that names can be generated.

--- parser/_choices.py
+++ parser/_choices.py
@@ -3,4 +3,7 @@ from parser import _keywords as k
 from parser import _repeats as rep
 from pyleri import Choice, Keyword
 
+Math = Choice(t.Plus, t.Minus, t.Multiply, t.Divide, t.Not, t.And, t.Or)
+Buttons = Choice(*[Keyword(f"Button{i}") for i in range(4)])
+HeldKey = Choice(k.Null, rep.Modkey)
 # Modkey = Choice(rep.Key, k.ShiftMask, k.ControlMask, *[Keyword(f"Mod{i}Mask") for i in range(1,6)])
