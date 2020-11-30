bf28208062e86806738b77f1cfcb3d55ba2a1405
dk949 <david2000katz@gmail.com>
2020-11-30 22:44:46 +0000

    Made extraction look a bit better. Made a dict wth the keysyms.

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,4 +1,4 @@
-from parser import treeParser
+from extractor import _Xkeysym
 
 
 # keyBindings[] holds:
@@ -7,6 +7,25 @@ from parser import treeParser
 #       key pressed,
 #       function to be called(args to function)
 
-def extract(tree):
-    tokens = treeParser.view_parse_tree(tree)
+# click event mask button function argument
 
+
+def extract_mouse(binding_list):
+    for i in binding_list:
+        print(i)
+
+
+def extract_key(binding_list):
+    print("modifier                     key        function        argument")
+    for binding in binding_list:
+        for i, element in enumerate(binding):
+            if i < len(binding) - 1:
+                if i == 0:
+                    for letter in element:
+                        print(letter if letter != '|' else ' + ', end="")
+                    print(" +", end="")
+                elif element in _Xkeysym.xkeysym:
+                    print(_Xkeysym.xkeysym[element] + " + ->", end="")
+                print(end=" ")
+            else:
+                print(element)
--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 # 2020-11-15
 # dk949
 
+# importing parser will parse the cli arguments
 import parser
+import extractor
 
 
 def main():
-
+    # parser.parse() will return a list of key/mouse bindings dependant on the struct signature and grammar supplied
     mouse = parser.parse("static Button buttons[]", parser.grammar.MouseGrammar())
     key = parser.parse("static Key keys[]", parser.grammar.KeyGrammar())
 
-    for i in mouse:
-        print(i)
-
+    extractor.extract_mouse(mouse)
     print("")
 
-    for i in key:
-        print(i)
+    extractor.extract_key(key)
 
 
 if __name__ == '__main__':
